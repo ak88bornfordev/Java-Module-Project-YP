cf1d43e0daace02c3228db88d59be2d16e49d32c
Aidar <ak88bornfordev@gmail.com>
2026-06-28 16:38:00 +0300

    v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,47 @@
+package org.example;
+
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+
+        for (int i = 1; i <= 3; i++) {
+            // Запрашиваем название автомобиля
+            String name;
+            while (true) {
+                System.out.println("Введите название машины №" + i + ":");
+                name = scanner.next();
+                if (!name.isEmpty()) {
+                    break;
+                }
+                System.out.println("Ошибка: название не может быть пустым. Попробуйте снова.");
+            }
+
+            // Запрашиваем скорость автомобиля
+            int speed;
+            while (true) {
+                System.out.println("Введите скорость машины №" + i + ":");
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    if (speed > 0 && speed <= 250) {
+                        break;
+                    } else {
+                        System.out.println("Ошибка: скорость должна быть от 1 до 250 км/ч. Попробуйте снова.");
+                    }
+                } else {
+                    System.out.println("Ошибка: введите целое число. Попробуйте снова.");
+                    scanner.next(); // сбрасываем неверный ввод
+                }
+            }
+
+            Car car = new Car(name, speed);
+            race.checkLeader(car);
+        }
+
+        scanner.close();
+        System.out.println("Самая быстрая машина: " + race.getLeaderName());
     }
 }
